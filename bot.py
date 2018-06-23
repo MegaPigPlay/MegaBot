b5cc691ff34117c6cef03ddff44044534cb56f9d
MegaPigPlay <40526255+MegaPigPlay@users.noreply.github.com>
2018-06-23 21:51:43 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+from discord.ext.commands import Bot
+import asyncio
+import time
+
+Client = discord.Client ()
+client = commands.Bot (command_prefix = "+")
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name="+help | By MegaPig"))
+    print("Bot Is Ready")
+
+   
+@client.event
+async def on_message(message):
+    if message.content.startswith('+hello'):
+        await client.send_message(message.channel, """`Hello! Im MegaBot`
+**My Creator Is MegaPig!**""")
+    if message.content.startswith('+invite'):
+        await client.send_message(message.channel, "https://discordapp.com/api/oauth2/authorize?client_id=459490391146627073&permissions=8&scope=bot __You Can Invite Me All Days!__                                                **MegaPig#1576** Is My Creator!")
+
+    if message.content.startswith('+help'):
+        embed = discord.Embed(title="Command List", description="""
+__**Information Commands**__
+
++hello
++invite
+
+__**Music Command**__
+
++join""", color=0xe88af4)
+        await client.send_message(message.channel, embed=embed)
+
+                                  
+client.run("NDU5NDkwMzkxMTQ2NjI3MDcz.Dg29_w.dxSVxwf0G-iJlCzckdcVlItfCu8")
